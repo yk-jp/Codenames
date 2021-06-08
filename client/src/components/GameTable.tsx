25293981596df8d1fca2d227dcfb7d42501f915a
yk-jp <k82.y12@gmail.com>
2021-06-09 00:37:56 +0900

    created side bar.

--- client/src/components/GameTable.tsx
+++ client/src/components/GameTable.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 //components
 import TeamTable from './TeamTable';
 // interfaces
@@ -18,117 +18,63 @@ const GameTable: FC = (): JSX.Element => {
     team: "RED"
   };
 
+  //useState
+  const [active, setActive] = useState("");
+
+
+  //sidebar 
+  const show = (): void => {
+    active == "" ? setActive("active") : setActive("");
+  }
+
   return (
-    <div className="container-fluid h-100">
-      <div className="row mt-5">
-        <div className="col-2 d-flex">
+    <div className="container-fluid">
+      <div className="row mt-5 d-flex">
+        <div className="col">
           {/* teams */}
-          <div className="d-flex flex-column">
-            <div className="m-4">
-              <h5 className="text-center">TEAMS</h5>
+          <div id="sidebar" className={active}>
+            <div className="toggle-btn" onClick={() => show()}>
+              <span></span>
+              <span></span>
+              <span></span>
             </div>
-            <div className="d-flex">
-              <div className="mx-1 col">
-                <TeamTable table={blueTeamProps} />
-              </div>
-              <div className="col">
-                <TeamTable table={redTeamProps} />
-              </div>
-            </div>
+            <ul className="d-flex justify-content-center">
+              <li><TeamTable table={blueTeamProps} /></li>
+              <li><TeamTable table={redTeamProps} /></li>
+            </ul>
           </div>
         </div>
-
         {/* cards */}
         <div className="col-8">
           <div className="d-flex flex-column justify-content-center">
             {/* turn */}
             <div className="d-flex justify-content-center w-100">
-              <h5 className="align-left"><span className="text-primary">2</span>-<span className="text-danger">4</span></h5>
+              <h5 className=""><span className="text-primary">2</span>-<span className="text-danger">4</span></h5>
               <h5><span className="text-danger">RED's TURN</span></h5>
-
             </div>
-            <div className="row col-12">
+            <div className="d-flex">
+              <div className="card">
+                <h6>efe</h6>
+              </div>
+              <div className="card">
+                <h6>efe</h6>
+              </div>
+              <div className="card">
+                <h6>efe</h6>
+              </div>
+              <div className="card">
+                <h6>efe</h6>
+              </div>
+              <div className="card">
+                <h6>efe</h6>
+              </div>
 
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-            </div>
-            <div className="row">
-
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-            </div>
-            <div className="row">
-
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-            </div>
-            <div className="row">
-
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card ">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card">
-                <h6>efe</h6>
-              </div>
-              <div className="card ">
-                <h6>efe</h6>
-              </div>
             </div>
 
 
           </div>
 
         </div>
-        <div className="col">
-          defe
-        </div>
-
-
       </div>
 
 
