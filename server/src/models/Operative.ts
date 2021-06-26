c206adf6fc6c435df043726e6add7d8f18e40cb3
yk-jp <k82.y12@gmail.com>
2021-06-26 15:41:43 +0900

    removed lobby page. and updated game page to receive a player name

--- server/src/models/Operative.ts
+++ server/src/models/Operative.ts
@@ -3,8 +3,8 @@ import Player from './Player';
 
 export default class Operative extends Player {
 
-  constructor(name: string, team: string) {
-    super(name, team);
+  constructor(name: string, id: string, team: string) {
+    super(name, id, team);
   }
 
   // 
