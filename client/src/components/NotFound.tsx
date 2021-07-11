b3169e95067d48859419f0a1597980744bfd1d4b
yk-jp <k82.y12@gmail.com>
2021-07-11 20:33:43 +0900

    function of a session.

---
+++ client/src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const NotFound: FC = (): JSX.Element => {
+  return (
+    <div className="container py-5">
+      <h1>SORRY, PAGE NOT FOUND :)</h1>
+    </div>
+  );
+}
+
+export default NotFound;
