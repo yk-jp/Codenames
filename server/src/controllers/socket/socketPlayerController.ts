963b2060875e28e92f3562ec033d68b0c4f5cae6
yk-jp <k82.y12@gmail.com>
2021-07-19 00:51:47 +0900

    fixed querys for table and player

--- server/src/controllers/socket/socketPlayerController.ts
+++ server/src/controllers/socket/socketPlayerController.ts
@@ -1,22 +1,25 @@
 // model
 import Table from "../../models/Table";
 import Operative from "../../models/Operative";
+import Player from "../../models/Player";
 import ConvertJson from "../../models/utils/convertJson";
+import PlayersInstance from "../../interfaces/schema/Players";
 // query
-import { player_find, player_findAll, player_insert, player_delete, player_update } from "../../controllers/queries/PlayersQuery";
-import { table_find, table_insert, table_update, table_delete } from "../../controllers/queries/TablesQuery";
+import { player_find } from "../../controllers/queries/PlayersQuery";
 
 const socketPlayerController = (io: any, socket: any): void => {
-  socket.on("receive-player", (playerName: string, playerId: string, roomId: string) => {
-    let player: Operative = new Operative(playerName, playerId, "OPERATIVE", "NO TEAM");
-    player_find(playerId)
-      .then(data => {
-        player = ConvertJson.toPlayer(JSON.parse(data!.get("player")));
-        socket.emit("receive-player", JSON.stringify(player));
-      })
-      .catch(() => {
-        console.log("could not find a player");
-      });
+  socket.on("receive-player", async (playerId: string) => {
+    try {
+      const playerData: PlayersInstance | null = await player_find(playerId);
+
+      if (!playerData) throw new Error("player was not found");
+
+      const player: Player = ConvertJson.toPlayer(JSON.parse(playerData.get("player")));
+      socket.emit("receive-player", JSON.stringify(player));
+
+    } catch (err) {
+      console.log(err);
+    }
   })
 }
 
