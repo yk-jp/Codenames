a9502a0109f290c6edacc1b8be718a73ed81d796
yk-jp <k82.y12@gmail.com>
2021-08-13 23:26:37 +0900

    deploy settings

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,36 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-//URI for fetching 25 words data from an endpoint.
-const WORD_URI: string = "http://localhost:8000/words";
-
-
-// scket.io
-const server_home: string = "http://localhost:3001";
-const server_form: string = "http://localhost:3001/form";
-const server_game: string = "http://localhost:3001/game";
-
-// transition
-const page_home: string = "http://localhost:3000";
-const page_form: string = "http://localhost:3000/form";
-const page_game: string = "http://localhost:3000/game";
+// socket.io
+const server_form: string = process.env.SERVER_FORM || "http://localhost:3001/form";
+const server_game: string = process.env.SERVER_GAME || "http://localhost:3001/game";
 
 const SERVER = {
-  home: server_home,
   form: server_form,
   game: server_game
 }
 
-const PAGE = {
-  home: page_home,
-  form: page_form,
-  game: page_game
-}
-
 const config = {
-  server: SERVER,
-  page: PAGE,
-  word_URI: WORD_URI
+  server: SERVER
 }
 
 export default config;
