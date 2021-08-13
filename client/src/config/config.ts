21229ecc7f888b538cc521e403c0435c5a06a32e
yk-jp <k82.y12@gmail.com>
2021-08-14 00:51:53 +0900

    environment variables seem to start with REACT_APP_

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // socket.io
-const server_form: string = process.env.SERVER_FORM || "http://localhost:3001/form";
-const server_game: string = process.env.SERVER_GAME || "http://localhost:3001/game";
+const server_form: string = process.env.REACT_APP_SERVER_FORM || "http://localhost:3001/form";
+const server_game: string = process.env.REACT_APP_SERVER_GAME || "http://localhost:3001/game";
 
 const SERVER = {
   form: server_form,
